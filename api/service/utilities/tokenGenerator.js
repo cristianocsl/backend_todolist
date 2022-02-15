f24bc28ce38a3b92f4282ada40fde8a17da4cad5
cristianocsl <cslcristiano@gmail.com>
2022-02-15 20:10:57 -0300

    feat: adiciona funçao que gera token

---
+++ api/service/utilities/tokenGenerator.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
+const jwtConfig = { expiresIn: '14d', algorithm: 'HS256' };
+
+const tokenGenerator = (payload) => jwt.sign(payload, JWT_SECRET, jwtConfig);
+
+module.exports = tokenGenerator;
