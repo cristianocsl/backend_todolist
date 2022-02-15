bb32cd0c07435fa09c71bdc0ff193fcda488339b
cristianocsl <cslcristiano@gmail.com>
2022-02-15 20:51:10 -0300

    feat: cria index.js para importaçao de funçoes do service de usuario

---
+++ api/service/user/index.js
@@ -0,0 +1,9 @@
+const findUserByEmail = require('./findUserByEmail');
+const login = require('./login');
+const register = require('./register');
+
+module.exports = {
+  findUserByEmail,
+  login,
+  register,
+};
