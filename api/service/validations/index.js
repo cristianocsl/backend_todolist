b6d5acd9859011d134691cc19a23748dbd8ca961
cristianocsl <cslcristiano@gmail.com>
2022-02-15 21:07:56 -0300

    feat: adiciona validaçao dos campos de login e register com joi

---
+++ api/service/validations/index.js
@@ -0,0 +1,7 @@
+const validateUserRegister = require('./validateUserRegister');
+const validateLogin = require('./validateLogin');
+
+module.exports = {
+  validateUserRegister,
+  validateLogin,
+};
