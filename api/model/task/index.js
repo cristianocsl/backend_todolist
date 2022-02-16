65ec01d7ab211a73c58563933c45177783094ad5
cristianocsl <cslcristiano@gmail.com>
2022-02-16 14:58:38 -0300

    refactor: exclui arquivos desnecessarios

--- api/model/task/index.js
+++ api/model/task/index.js
@@ -2,12 +2,10 @@ const createTask = require('./createTask');
 const deleteTask = require('./deleteTask');
 const getAllTasks = require('./getAllTasks');
 const updateTask = require('./updateTask');
-const findTaskById = require('./findTaskById');
 
 module.exports = {
   createTask,
   deleteTask,
   getAllTasks,
   updateTask,
-  findTaskById,
 };
