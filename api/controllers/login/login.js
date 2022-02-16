0768aa24b87b4011da4e78fb4eb9a33ed7fc27e1
cristianocsl <cslcristiano@gmail.com>
2022-02-16 02:40:36 -0300

    feat: adiciona controller de login

---
+++ api/controllers/login/login.js
@@ -0,0 +1,16 @@
+const { OK } = require('http-status-codes').StatusCodes;
+const { validateLogin } = require('../../service/validations');
+const { login: loginWith } = require('../../service/user');
+
+const login = async (req, res, next) => {
+  const reqBody = req.body;
+  const { error } = await validateLogin(reqBody);
+  if (error) return next(error);
+  const token = await loginWith(reqBody);
+  return res.status(OK).json({
+    message: 'Login efetuado com sucesso!',
+    ...token,
+  });
+};
+
+module.exports = login;
