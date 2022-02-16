991773aeb95989be1515e5bf1bd46066b68cd346
cristianocsl <cslcristiano@gmail.com>
2022-02-16 18:46:25 -0300

    refactor: adapta funçao para converter data de criaçao da tarefa

--- api/service/task/getAllTasks.js
+++ api/service/task/getAllTasks.js
@@ -1,5 +1,8 @@
+const { outputDate } = require('../utilities/outputDate');
 const { getAllTasks: goToGetAllTasks } = require('../../model/task');
 
-const getAllTasks = async (userId) => goToGetAllTasks(userId);
-
+const getAllTasks = async (userId) => {
+  const tasks = await goToGetAllTasks(userId);
+  return outputDate(tasks);
+};
 module.exports = getAllTasks;
