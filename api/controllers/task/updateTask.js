70db040ff2d30dc75e3dda47eef21b296e17cddd
cristianocsl <cslcristiano@gmail.com>
2022-02-16 17:05:27 -0300

    feat: cria controller para atualizaçao da tarefa

---
+++ api/controllers/task/updateTask.js
@@ -0,0 +1,20 @@
+const { OK } = require('http-status-codes').StatusCodes;
+const { updateTask: update } = require('../../service/task');
+const { validateTask } = require('../../service/validations');
+
+const updateTask = async (req, res, next) => {
+  const { body, params: { taskId } } = req;
+
+  const { error } = await validateTask(body);
+
+  if (error) return next(error);
+
+  try {
+    await update(taskId, body);
+    return res.status(OK).json({ message: 'Task updated successfully' });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = updateTask;
