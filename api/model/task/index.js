e54ad14416dcb0282b0da64fe04fab29361f3c71
cristianocsl <cslcristiano@gmail.com>
2022-02-16 17:06:10 -0300

    refactor: inclui findbytaskid() no index.js para importar arquivos

--- api/model/task/index.js
+++ api/model/task/index.js
@@ -2,10 +2,12 @@ const createTask = require('./createTask');
 const deleteTask = require('./deleteTask');
 const getAllTasks = require('./getAllTasks');
 const updateTask = require('./updateTask');
+const findTaskById = require('./findTaskById');
 
 module.exports = {
   createTask,
   deleteTask,
   getAllTasks,
   updateTask,
+  findTaskById,
 };
