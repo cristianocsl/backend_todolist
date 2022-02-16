dd8b5978ba101473b06d2b825f1c0a8ac31e6a5b
cristianocsl <cslcristiano@gmail.com>
2022-02-16 17:02:04 -0300

    refactor: atualiza mensagens do middleware de erro

--- api/error/msgCodeError.js
+++ api/error/msgCodeError.js
@@ -13,17 +13,9 @@ const INVALID_TOKEN = { code: UNAUTHORIZED, message: 'Expired or invalid token' 
 
 const USER_DOES_NOT_EXIST = { code: NOT_FOUND, message: 'User does not exist' };
 
-const CATEGORY_NOT_FOUND = { code: BAD_REQUEST, message: '"categoryIds" not found' };
-
-const POST_DOES_NOT_EXIST = { code: NOT_FOUND, message: 'Post does not exist' };
+const TASK_NOT_EXIST = { code: NOT_FOUND, message: 'Task does not exist' };
 
 const UNAUTHORIZED_USER = { code: UNAUTHORIZED, message: 'Unauthorized user' };
-
-const CANNOT_BE_EDITADED = { code: BAD_REQUEST, message: 'Categories cannot be edited' };
-
-const TITLE_REQUIRED = { code: BAD_REQUEST, message: '"title" is required' };
-
-const CONTENT_REQUIRED = { code: BAD_REQUEST, message: '"content" is required' };
 
 module.exports = {
   INCORRECT_LOGIN,
@@ -31,10 +23,6 @@ module.exports = {
   TOKEN_NOT_FOUND,
   INVALID_TOKEN,
   USER_DOES_NOT_EXIST,
-  CATEGORY_NOT_FOUND,
-  POST_DOES_NOT_EXIST,
+  TASK_NOT_EXIST,
   UNAUTHORIZED_USER,
-  CANNOT_BE_EDITADED,
-  TITLE_REQUIRED,
-  CONTENT_REQUIRED,
 };
