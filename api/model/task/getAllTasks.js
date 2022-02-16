d2acdb3938b59be3de79dd9d7aed854a87589226
cristianocsl <cslcristiano@gmail.com>
2022-02-16 13:25:22 -0300

    refactor: adapta funçao para retornar todas as tarefas a partir do id do usuario

--- api/model/task/getAllTasks.js
+++ api/model/task/getAllTasks.js
@@ -1,8 +1,11 @@
+const { ObjectId } = require('mongodb');
 const { client } = require('../connection');
+const { isValid } = require('../util/validateObjectId');
 
-const getAllTasks = async () => {
+const getAllTasks = async (userId) => {
+  await isValid(userId);
   const db = client.db('todoDatabase');
-  return db.collection('tasks').find().toArray();
+  return db.collection('tasks').find({ _id: ObjectId(userId) }).toArray();
 };
 
 module.exports = getAllTasks;
