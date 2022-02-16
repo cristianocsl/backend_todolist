23351cd94c5d2d6b285b2dbe40bbe4a428a18f1e
cristianocsl <cslcristiano@gmail.com>
2022-02-16 12:37:40 -0300

    feat: adiciona rotas futuras para tarefas

--- api/controllers/root.js
+++ api/controllers/root.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const routerUser = require('./user/router');
 const routerLogin = require('./login/router');
+const routerTask = require('./task/router');
 
 const root = express.Router({ mergeParams: true });
 
 root.use('/user', routerUser);
 root.use('/login', routerLogin);
+root.use('/task', routerTask);
 
 module.exports = root;
