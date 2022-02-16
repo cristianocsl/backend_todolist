376894d384d6ad1b0b0419df4a9b327d46217be4
cristianocsl <cslcristiano@gmail.com>
2022-02-16 12:06:05 -0300

    feat: adiciona arquivo index.js nas camadas model e controller (para task) para importar arquivos

---
+++ api/controllers/task/index.js
@@ -0,0 +1,11 @@
+const createTask = require('./createTask');
+const deleteTask = require('./deleteTask');
+const getAllTasks = require('./getAllTasks');
+const updateTask = require('./updateTask');
+
+module.exports = {
+  createTask,
+  deleteTask,
+  getAllTasks,
+  updateTask,
+};
