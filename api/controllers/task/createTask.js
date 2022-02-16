81715ed696184de8414f633c848db79d5b0aff12
cristianocsl <cslcristiano@gmail.com>
2022-02-16 19:48:09 -0300

    refactor: adiciona o nome do usuario a tarefa criada

--- api/controllers/task/createTask.js
+++ api/controllers/task/createTask.js
@@ -3,14 +3,14 @@ const { createTask: create } = require('../../service/task');
 const { validateTask } = require('../../service/validations');
 
 const createTask = async (req, res, next) => {
-  const { body, user: { _id } } = req;
+  const { body, user: { _id, name } } = req;
 
   const { error } = await validateTask(body);
 
   if (error) return next(error); 
 
   try {
-    const recipe = await create({ ...body, userId: _id });
+    const recipe = await create({ ...body, userId: _id, name });
     
     return res.status(CREATED).json(recipe);
   } catch (err) { next(err); }
