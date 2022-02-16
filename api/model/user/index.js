72c55ce4cfeabcb1f6ca9402a1859e918f9f92fd
cristianocsl <cslcristiano@gmail.com>
2022-02-16 03:00:12 -0300

    revert: reverter commit para retirar autenticaçao da rota de login

--- api/model/user/index.js
+++ api/model/user/index.js
@@ -1,7 +1,9 @@
 const register = require('./register');
 const findUserByEmail = require('./findUserByEmail');
+const login = require('./login');
 
 module.exports = {
   register,
   findUserByEmail,
+  login,
 };
