099e01ba5db904fdb28c34e47ee8ffbeea7f9ad7
cristianocsl <cslcristiano@gmail.com>
2022-02-16 02:30:15 -0300

    feat: adiciona service de login

---
+++ api/service/user/login.js
@@ -0,0 +1,19 @@
+const md5 = require('md5');
+const { findUserByEmail } = require('../../model/user');
+const { ApiError: { SendToErrorMiddleware } } = require('../../error/apiError');
+const { INCORRECT_LOGIN } = require('../../error/msgCodeError');
+const { tokenGenerator } = require('../utilities/tokenGenerator');
+
+const login = async ({ email, password: inputPassword }) => {
+  const user = await findUserByEmail(email);
+
+  if (!user || md5(inputPassword) !== user.password) return SendToErrorMiddleware(INCORRECT_LOGIN);
+
+  const { _id, name } = user;
+
+  const token = tokenGenerator({ _id, name, email });
+
+  return { token };
+};
+
+module.exports = login;
