070a953ebdac3950ccaf622a9913e3152c58ed31
cristianocsl <cslcristiano@gmail.com>
2022-02-18 20:30:21 -0300

    feat: adiciona campo de status para a tarefa

--- api/service/validations/validateTask.js
+++ api/service/validations/validateTask.js
@@ -2,6 +2,7 @@ const Joi = require('joi');
 
 const SCHEMA = Joi.object({
   task: Joi.string().not().empty().required(),
+  status: Joi.string().not().empty().required(),
 });
 
 const validateTask = async (body) => SCHEMA.validate(body);
