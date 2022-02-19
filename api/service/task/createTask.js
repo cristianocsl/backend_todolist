b37e5ff9fa91bd4c822c7223bb1f5102e41ff302
cristianocsl <cslcristiano@gmail.com>
2022-02-18 21:17:02 -0300

    feat: adiciona chave status para o BD

--- api/service/task/createTask.js
+++ api/service/task/createTask.js
@@ -2,7 +2,8 @@ const { createTask: create } = require('../../model/task');
 
 const createTask = async (taskContent) => {
   const createdAt = new Date().toISOString();
-  await create({ ...taskContent, createdAt });
+  const status = taskContent.status || 'To do';
+  await create({ ...taskContent, createdAt, status });
 };
 
 module.exports = createTask;
