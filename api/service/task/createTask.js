692b27b78bab483e3f484fc43b9cfcd41fb6697c
cristianocsl <cslcristiano@gmail.com>
2022-02-20 03:58:23 -0300

    fix: ajusta formato da data de criaçao

--- api/service/task/createTask.js
+++ api/service/task/createTask.js
@@ -1,10 +1,11 @@
 const { createTask: create } = require('../../model/task');
+const { outputDate } = require('../utilities/outputDate');
 
 const createTask = async (taskAndUserId) => {
   const createdAt = new Date().toISOString();
   const status = taskAndUserId.status || 'To do';
   const { _id } = await create({ ...taskAndUserId, createdAt, status });
-  return { _id, ...taskAndUserId, createdAt, status };
+  return outputDate({ _id, ...taskAndUserId, createdAt, status });
 };
 
 module.exports = createTask;
