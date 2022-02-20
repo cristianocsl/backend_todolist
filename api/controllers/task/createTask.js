6d4450ba3310325558564c2c509b916a155f4f95
cristianocsl <cslcristiano@gmail.com>
2022-02-19 21:47:28 -0300

    refactor: retira o campo 'name' na coleçao tasks

--- api/controllers/task/createTask.js
+++ api/controllers/task/createTask.js
@@ -3,14 +3,14 @@ const { createTask: create } = require('../../service/task');
 const { validateTask } = require('../../service/validations');
 
 const createTask = async (req, res, next) => {
-  const { body, user: { _id, name } } = req;
+  const { body, user: { _id } } = req;
 
   const { error } = await validateTask(body);
 
   if (error) return next(error); 
 
   try {
-    const recipe = await create({ ...body, userId: _id, name });
+    const recipe = await create({ ...body, userId: _id });
     
     return res.status(CREATED).json(recipe);
   } catch (err) { next(err); }
