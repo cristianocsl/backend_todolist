edc054a84fe9dd7863224325e1d679738eca11df
cristianocsl <cslcristiano@gmail.com>
2022-03-03 13:18:51 -0300

    feat: adiciona frases de validaçao em portugues

--- api/service/validations/validateLogin.js
+++ api/service/validations/validateLogin.js
@@ -1,10 +1,16 @@
 const Joi = require('joi');
 
+const ERR_LOGIN = {
+  'string.base': 'Preencha apenas com letras.',
+  'string.empty': 'Este campo não pode ser vazio.',
+  'string.min': 'Utilize no mínimo de 3 caracteres.',
+  'any.required': 'Este campo é obrigatório.',
+};
+
 const SCHEMA = {
-  email: Joi.string().email().not().empty()
-    .required(),
-  password: Joi.string().min(6).not().empty()
-    .required(),
+  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+  .messages(ERR_LOGIN),
+  password: Joi.string().min(6).required().messages(ERR_LOGIN),
 };
 
 const validateLogin = async (body) => Joi.object(SCHEMA).validate(body);
--- api/service/validations/validateUserRegister.js
+++ api/service/validations/validateUserRegister.js
@@ -1,12 +1,22 @@
 const Joi = require('joi');
 
+const STRONG_PASSWORD = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{6,}$/;
+
+const ERR_REGISTER = {
+  'any.required': 'Este campo é obrigatório.',
+  'string.base': 'Preencha apenas com letras.',
+  'string.empty': 'Este campo não pode ser vazio.',
+  'string.min': 'Utilize no mínimo de 6 caracteres.',
+  'string.email': 'Preencha este campo com um email válido.',
+  'string.pattern': 'Preencha com pelo menos: 1 letra maiúscula, 1 número e 1 caractere especial.',
+};
+
 const SCHEMA = Joi.object({
-  name: Joi.string().min(8).not().empty()
+  name: Joi.string().min(3).not().empty()
     .required(),
-  email: Joi.string().email().not().empty()
-    .required(),
-  password: Joi.string().min(6).not().empty()
-    .required(),
+  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    .messages(ERR_REGISTER),
+  password: Joi.string().pattern(STRONG_PASSWORD).messages(ERR_REGISTER).required(),
 });
 
 const validateUserRegister = async (body) => SCHEMA.validate(body);
