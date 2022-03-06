6a5745ad693c4392caa1c38d33d76c0826530119
cristianocsl <cslcristiano@gmail.com>
2022-03-06 00:48:04 -0300

    refactor: git pull origin main

--- api/error/msgCodeError.js
+++ api/error/msgCodeError.js
@@ -3,19 +3,19 @@ const {
   CONFLICT,
   UNAUTHORIZED, NOT_FOUND } = require('http-status-codes').StatusCodes;
 
-const INCORRECT_LOGIN = { code: BAD_REQUEST, message: 'Email ou senha incorretos!' };
-
-const EMAIL_EXISTING = { code: CONFLICT, message: 'Usuário já está registrado!' };
-
-const TOKEN_NOT_FOUND = { code: UNAUTHORIZED, message: 'Token não encontrado!' };
-
-const INVALID_TOKEN = { code: UNAUTHORIZED, message: 'Token expirado ou inválido!' };
-
-const USER_DOES_NOT_EXIST = { code: NOT_FOUND, message: 'Usuário não encontrado!' };
-
-const TASK_NOT_EXIST = { code: NOT_FOUND, message: 'Tarefa não existente!' };
-
-const UNAUTHORIZED_USER = { code: UNAUTHORIZED, message: 'Usuário não autorizado!' };
+  const INCORRECT_LOGIN = { code: BAD_REQUEST, message: 'Email or password incorrect' };
+  
+  const EMAIL_EXISTING = { code: CONFLICT, message: 'User already registered' };
+  
+  const TOKEN_NOT_FOUND = { code: UNAUTHORIZED, message: 'Token not found' };
+  
+  const INVALID_TOKEN = { code: UNAUTHORIZED, message: 'Expired or invalid token' };
+  
+  const USER_DOES_NOT_EXIST = { code: NOT_FOUND, message: 'User does not exist' };
+  
+  const TASK_NOT_EXIST = { code: NOT_FOUND, message: 'Task does not exist' };
+  
+  const UNAUTHORIZED_USER = { code: UNAUTHORIZED, message: 'Unauthorized user' };
 
 module.exports = {
   INCORRECT_LOGIN,
