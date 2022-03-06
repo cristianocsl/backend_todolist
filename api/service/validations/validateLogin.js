69bd8fc93dbc1a558948066e2a3dca8e34184952
cristianocsl <cslcristiano@gmail.com>
2022-03-05 22:36:03 -0300

    refactor: retorna a configuraçao inicial do joi em ingles

--- api/service/validations/validateLogin.js
+++ api/service/validations/validateLogin.js
@@ -1,16 +1,10 @@
 const Joi = require('joi');
 
-const ERR_LOGIN = {
-  'string.base': 'Preencha apenas com letras.',
-  'string.empty': 'Este campo não pode ser vazio.',
-  'string.min': 'Utilize no mínimo de 6 caracteres.',
-  'any.required': 'Este campo é obrigatório.',
-};
-
 const SCHEMA = {
-  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
-  .messages(ERR_LOGIN),
-  password: Joi.string().min(6).required().messages(ERR_LOGIN),
+  email: Joi.string().email().not().empty()
+    .required(),
+  password: Joi.string().length(6).not().empty()
+    .required(),
 };
 
 const validateLogin = async (body) => Joi.object(SCHEMA).validate(body);
--- api/service/validations/validateUserRegister.js
+++ api/service/validations/validateUserRegister.js
@@ -1,28 +1,12 @@
 const Joi = require('joi');
 
-const STRONG_PASSWORD = /^(?=.*\d)(?=.*[a-z])[0-9a-zA-Z]{6,}$/;
-
-const ERR_NAME = {
-  'any.required': 'Este campo é obrigatório.',
-  'string.base': 'Preencha apenas com letras.',
-  'string.empty': 'Este campo não pode ser vazio.',
-  'string.min': 'Utilize no mínimo de 3 caracteres.',
-};
-
-const ERR_REGISTER = {
-  'any.required': 'Este campo é obrigatório.',
-  'string.base': 'Preencha apenas com letras.',
-  'string.empty': 'Este campo não pode ser vazio.',
-  'string.min': 'Utilize no mínimo de 6 caracteres.',
-  'string.email': 'Preencha este campo com um email válido.',
-  'string.pattern': 'Preencha com pelo menos: 1 letra maiúscula, 1 número e 1 caractere especial.',
-};
-
 const SCHEMA = Joi.object({
-  name: Joi.string().min(3).messages(ERR_NAME).required(),
-  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
-    .messages(ERR_REGISTER),
-  password: Joi.string().pattern(STRONG_PASSWORD).messages(ERR_REGISTER).required(),
+  name: Joi.string().min(8).not().empty()
+    .required(),
+  email: Joi.string().email().not().empty()
+    .required(),
+  password: Joi.string().length(6).not().empty()
+    .required(),
 });
 
 const validateUserRegister = async (body) => SCHEMA.validate(body);
