a50aa19895b3163e7fffbb13784f5d39cf2bdc41
cristianocsl <cslcristiano@gmail.com>
2022-03-06 11:36:31 -0300

    fix: refatora mensagens de erro e logica do service login

--- api/service/user/login.js
+++ api/service/user/login.js
@@ -1,13 +1,15 @@
 const md5 = require('md5');
 const { findUserByEmail } = require('../../model/user');
 const { ApiError: { SendToErrorMiddleware } } = require('../../error/apiError');
-const { INCORRECT_LOGIN } = require('../../error/msgCodeError');
+const { INCORRECT_LOGIN, USER_DOES_NOT_EXIST } = require('../../error/msgCodeError');
 const { tokenGenerator } = require('../utilities/tokenGenerator');
 
 const login = async ({ email, password: inputPassword }) => {
   const user = await findUserByEmail(email);
 
-  if (!user || md5(inputPassword) !== user.password) return SendToErrorMiddleware(INCORRECT_LOGIN);
+  if (!user) return SendToErrorMiddleware(USER_DOES_NOT_EXIST);
+
+  if (md5(inputPassword) !== user.password) return SendToErrorMiddleware(INCORRECT_LOGIN);
 
   const { _id, name } = user;
 
