465c1ccedc509ef8d6dee86e71ee48c5680da80e
cristianocsl <cslcristiano@gmail.com>
2022-03-15 02:11:47 -0300

    fix: remove console.log

--- api/model/task/deleteTask.js
+++ api/model/task/deleteTask.js
@@ -5,8 +5,7 @@ const { isValid } = require('../util/validateObjectId');
 const deleteTask = async (_id) => {
   await isValid(_id);
   const db = client.db('todoDatabase');
-  const del = await db.collection('tasks').deleteOne({ _id: ObjectId(_id) });
-  console.log(del);
+  await db.collection('tasks').deleteOne({ _id: ObjectId(_id) });
 };
 
 module.exports = deleteTask;
