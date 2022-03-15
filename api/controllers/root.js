71bee15a09d0b058448281075aa4e0d335e2f768
cristianocsl <cslcristiano@gmail.com>
2022-03-15 02:10:38 -0300

    feat: pull a partir da main

--- api/controllers/root.js
+++ api/controllers/root.js
@@ -3,9 +3,11 @@ const express = require('express');
 const routerUser = require('./user/router');
 const routerLogin = require('./login/router');
 const routerTask = require('./task/router');
+const routerWakeup = require('./wakeup/router');
 
 const root = express.Router({ mergeParams: true });
 
+root.use('/wakeup', routerWakeup);
 root.use('/register', routerUser);
 root.use('/login', routerLogin);
 root.use('/task', routerTask);
