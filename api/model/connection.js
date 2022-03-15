c43bae25d8117883d6660221f07ae234c093f8df
cristianocsl <cslcristiano@gmail.com>
2022-03-15 01:55:06 -0300

    feat: pull a partir da main

--- api/model/connection.js
+++ api/model/connection.js
@@ -1,22 +1,6 @@
-// const { MongoClient } = require('mongodb');
-
-// require('dotenv').config();
-
-// // const { HOSTNAME } = process.env;
-// // const MONGO_DB_URL_LOCAL = `mongodb://${HOSTNAME}:27017`;
-// const { MONGO_DB_URL } = process.env;
-
-// const OPTIONS = {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// };
-
-// // const client = new MongoClient(MONGO_DB_URL_LOCAL, OPTIONS);
-// const client = new MongoClient(MONGO_DB_URL, OPTIONS);
-
 const { mongoClientOption } = require('./mongoClientMode');
 
-const client = mongoClientOption.heroku;
+const client = mongoClientOption.localhost;
 
 const bootStrap = async () => {
   try {
