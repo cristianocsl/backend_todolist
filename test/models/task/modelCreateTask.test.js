3f7adc872bf8b51c35158a869049112ae937e83e
cristianocsl <cslcristiano@gmail.com>
2022-04-21 13:04:46 -0300

    feat: pull a partir da main

--- test/models/task/modelCreateTask.test.js
+++ test/models/task/modelCreateTask.test.js
@@ -1,34 +1,43 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable mocha/no-mocha-arrows */
 const sinon = require('sinon');
 const { expect } = require('chai');
 const { MongoClient } = require('mongodb');
-const { getConnection } = require('../modelConnection.test');
+const { getConnection } = require('../modelConnection');
 
-// const { client } = require('../../../api/model/connection');
-const createTask = require('../../../api/model/task/createTask');
+const { createTask } = require('../../../api/model/task');
 
 describe('Insere uma nova task', () => {
   let connectionMock;
+  
   const payloadTask = { task: 'ir ao mercado' };
-
+  
   before(async () => {
     connectionMock = await getConnection();
     sinon.stub(MongoClient, 'connect').resolves(connectionMock);
   });
-  
+
   after(async () => {
     MongoClient.connect.restore();
   });
   
   describe('quando é inserido com sucesso', () => {
-    it('deve existir um filme com o título cadastrado', async () => {
-      await createTask(payloadTask);
-      const createdTask = await connectionMock
-        .db('todoDatabase')
-        .collection('tasks')
-        .findOne({ task: payloadTask.task });
-      // eslint-disable-next-line no-unused-expressions
-      expect(createdTask).to.be.not.null;
+    it('tal objeto possui o "_id" da nova tarefa inserida', async () => {
+      const response = await createTask({ ...payloadTask });
+      expect(response).to.have.a.property('_id');
+    });
+
+    it('esperado retornar um objeto', async () => {
+      const response = await createTask({ ...payloadTask });
+      expect(response).to.be.an('object');
+    });
+
+    it('deve existir uma tarefa cadastrada', async () => {
+      await createTask({ ...payloadTask });
+      const db = await connectionMock.db('todoDatabase');
+      const collect = await db.collection('tasks');
+      const task = await collect.find().toArray();
+      expect(task).to.be.not.null;
     });
   });
 });
